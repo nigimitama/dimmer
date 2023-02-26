91996359dff7dae9904355e93d870a489130c82c
mitama <nigimitama16@gmail.com>
2023-02-26 23:41:51 +0900

    fix codes for packageing app

--- monitor-controller.js
+++ monitor-controller.js
@@ -1,14 +1,17 @@
 const { PythonShell } = require("python-shell");
+const path = require("path");
 
 exports.getLuminance = async () => {
   // return: array[string]
-  const result = await PythonShell.run("python_modules/get_luminance.py")
-  return result
+  const scriptPath = path.join(__dirname, "python_modules/get_luminance.py");
+  const result = await PythonShell.run(scriptPath);
+  return result;
 };
 
 exports.setLuminance = async (value = null) => {
   const options = {
-    args: [value]
-  }
-  return await PythonShell.run("python_modules/set_luminance.py", options)
+    args: [value],
+  };
+  const scriptPath = path.join(__dirname, "python_modules/set_luminance.py");
+  return await PythonShell.run(scriptPath, options);
 };

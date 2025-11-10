20aa5ad96518a07f707931356a8ac1f4ca27d664
mitama <nigimitama16@gmail.com>
2025-11-11 07:48:03 +0900

    ci: update build pipeline to support multiple OS

--- build_exe.ps1
+++ build_exe.ps1
@@ -1,11 +1,12 @@
 # Windows用のexeファイルの生成
+# CIでもビルドしているので、こちらはローカルテスト用
 uv run `
---group dev pyinstaller `
---name dimmer `
---icon="assets/icon-dark.ico" `
---add-data "assets/icon-dark.ico;assets/" `
---add-data "assets/icon-light.ico;assets/" `
---collect-data schedule `
---hidden-import=schedule `
---noconsole --onefile --clean `
-main.py
+    --group dev pyinstaller `
+    --name dimmer `
+    --icon="assets/icon-dark.ico" `
+    --add-data "assets/icon-dark.ico;assets/" `
+    --add-data "assets/icon-light.ico;assets/" `
+    --collect-data schedule `
+    --hidden-import=schedule `
+    --noconsole --onefile --clean `
+    main.py
